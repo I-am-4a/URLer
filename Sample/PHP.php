04709c6bde14569ec2e39da55e5fd61af9efac73
I-am-4a <ts3@f5.si>
2018-02-21 18:41:36 +0900

    Sample Version.1

---
+++ Sample/PHP.php
@@ -0,0 +1,39 @@
+<?php
+	// [PHP] URLer post request sample
+	// http://urler.cf/
+	// by I_am_4a
+	// http://www.iam4a.ml/
+	
+	// post(string $url, array $data, array $header)
+	function post($url, $data, $header) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		$result = curl_exec($ch);
+		curl_close($ch);
+		
+		return $result;
+	}
+	
+	// Post data
+	$data = array(
+		"url" => "YOUR LONG LONG URL"
+	);
+	
+	// Post URL
+	$url = "http://urler.cf/bin/urlgen.php";
+	
+	// Post header
+	$header = array(
+		"Content-Type" => "application/json"
+	);
+	
+	// Post request
+	$res = post($url, $data, $header);
+	
+	// Result
+	var_dump($res);
+?>
---
+++ Sample/Python.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# [Python] URLer post request sample
+# http://urler.cf/
+# by I_am_4a
+# http://www.iam4a.ml/
+
+from requests import post
+import json as j
+
+# Request URL
+url = "http://urler.cf/bin/urlgen.php"
+
+# Post Data
+data = {"url": "YOUR LONG LONG URL"}
+
+# Headers
+headers = {"Content-Type": "application/json"}
+
+# Request start
+with post(url, data=j.dumps(data), headers=headers) as res:
+	
+	# Get JSON
+	json = res.json()
+	
+	# Print
+	print(json)
+	
+	print("-"*30)
+	
+	# Short URL
+	print(json["data"]["shortUrl"])
